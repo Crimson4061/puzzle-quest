aef5ea1a11c947dc888a1ab8d6bec0e393338db1
Crimson4061 <58094301+Crimson4061@users.noreply.github.com>
2025-09-17 21:25:09 -0400

    Change all four-space indents to tabs and delete i-hate-unity.gif

--- js/resources.js
+++ js/resources.js
@@ -1,15 +1,15 @@
 function loadRes() {
-    for (let cat in res) {
-        for (let id in res[cat]) {
-            let img = new Image();
-            img.src = res[cat][id];
-            res[cat][id] = img;
-        }
-    }
+	for (let cat in res) {
+		for (let id in res[cat]) {
+			let img = new Image();
+			img.src = res[cat][id];
+			res[cat][id] = img;
+		}
+	}
 }
 
 let res = {
-    images: {
-        "no-unity": "res/i-hate-unity.gif",
-    },
+	images: {
+		"no-unity": "res/i-hate-unity.gif",
+	},
 }
